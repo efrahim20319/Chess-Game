7b7452d658122a50fc1390db4fe97c2b0d0438e9
Efrahim Tamaku <efrahimtks@gmail.com>
2022-04-04 13:38:46 +0100

    Modificadoo codigo do Rei

--- assets/js/pecas/Rei.js
+++ assets/js/pecas/Rei.js
@@ -6,10 +6,7 @@ export class Rei extends Peca {
 		super();
 	}
 
-	mostrarDisponiveis() {
-		Jogo.desmarcarTudo();
-		this.elemento.setAttribute("data-marcador", "");
-		const [linha, coluna] = Jogo.obetrPosicao(this.elemento.parentNode);
+	casasAhVolta(linha, coluna) {
 		const casa_a_tras = Jogo.obterCasa(linha + 1, coluna);
 		const casa_a_frente = Jogo.obterCasa(linha - 1, coluna);
 		const casa_a_direita = Jogo.obterCasa(linha, coluna + 1);
@@ -19,7 +16,7 @@ export class Rei extends Peca {
 		const casa_baixo_direita = Jogo.obterCasa(linha + 1, coluna + 1);
 		const casa_baixo_esquerda = Jogo.obterCasa(linha + 1, coluna - 1);
 
-		const casasAhmarcar = [
+		return [
 			casa_a_tras,
 			casa_a_frente,
 			casa_a_direita,
@@ -29,7 +26,13 @@ export class Rei extends Peca {
 			casa_baixo_direita,
 			casa_baixo_esquerda,
 		];
+	}
 
+	mostrarDisponiveis() {
+		Jogo.desmarcarTudo();
+		this.elemento.setAttribute("data-marcador", "");
+		const [linha, coluna] = Jogo.obetrPosicao(this.elemento.parentNode);
+		const casasAhmarcar = this.casasAhVolta(linha ,coluna)
 		Jogo.marcarGrupo(casasAhmarcar);
 		Jogo.prepararMovimento();
 	}
